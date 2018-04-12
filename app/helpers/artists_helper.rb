618237e30308a2dda57f51c4e2b9998190d94222
Yomaira Escano <yjescano@gmail.com>
2018-04-12 13:12:20 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
 
   def display_artist(song)
     # @song = song.artist
-    if song.artist
+    if song.artist != "" && song.artist.name != ""
       link_to song.artist.name, artist_path(song.artist)
     else
       link_to "Add Artist", edit_song_path(song)
